7b1629d8f2919f5c3ab98c17832190cc1cf8d5f1
imlokesh <me@imlokesh.com>
2022-09-11 09:54:51 +0530

    HttpMethodTest

--- IMLokesh.HttpClient.Tests/HttpClientTests.cs
+++ IMLokesh.HttpClient.Tests/HttpClientTests.cs
@@ -4,12 +4,12 @@ namespace IMLokesh.HttpClient.Tests
     {
         [Theory]
         [MemberData(nameof(HttpVersionTestData))]
-        public async Task HttpVersionIsCorrect(Version version, string result)
+        public async Task HttpVersionIsCorrect(Version version, string expectedProtocol)
         {
             var http = new Http() { Version = version };
             var res = await http.RequestAsync("https://http2.pro/api/v1");
-            var protocol = res.JObject["protocol"]?.Value<string>();
-            Assert.Equal(result, protocol);
+            var actualProtocol = res.JObject["protocol"]?.Value<string>();
+            Assert.Equal(expectedProtocol, actualProtocol);
         }
 
         public static IEnumerable<object[]> HttpVersionTestData()
@@ -18,6 +18,26 @@ namespace IMLokesh.HttpClient.Tests
             yield return new object[] { null, "HTTP/1.1" };
             yield return new object[] { new Version(1, 0), "HTTP/1.0" };
             yield return new object[] { new Version(1, 1), "HTTP/1.1" };
+        }
+
+        [Theory]
+        [MemberData(nameof(HttpMethodTestData))]
+        public async Task HttpMethodIsCorrect(HttpMethod method, string expectedMethod)
+        {
+            var http = new Http();
+            var res = await http.RequestAsync("https://httpbin.org/anything", method);
+            var actualMethod = res.JObject["method"]?.Value<string>();
+            Assert.Equal(actualMethod, expectedMethod);
+        }
+
+        public static IEnumerable<object[]> HttpMethodTestData()
+        {
+            yield return new object[] { null, "GET" };
+            yield return new object[] { HttpMethod.Get, "GET" };
+            yield return new object[] { HttpMethod.Post, "POST" };
+            yield return new object[] { HttpMethod.Put, "PUT" };
+            yield return new object[] { HttpMethod.Patch, "PATCH" };
+            yield return new object[] { HttpMethod.Delete, "DELETE" };
         }
     }
 }
